d7a2ba7a193cc69ab1e4c3278b7e383e94ca56f9
Hidetoshi Yanagi <daruyanagi@gmail.com>
2012-03-07 02:37:31 +0900

    Initial Rease

---
+++ SoundKeyboard2012/MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SoundKeyboard2012
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelKeyData = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonReload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelKeyData
+            // 
+            this.labelKeyData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelKeyData.Font = new System.Drawing.Font("メイリオ", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelKeyData.Location = new System.Drawing.Point(12, 103);
+            this.labelKeyData.Name = "labelKeyData";
+            this.labelKeyData.Size = new System.Drawing.Size(360, 48);
+            this.labelKeyData.TabIndex = 0;
+            this.labelKeyData.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(20, 212);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(271, 26);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // buttonReload
+            // 
+            this.buttonReload.Location = new System.Drawing.Point(297, 212);
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.Size = new System.Drawing.Size(75, 26);
+            this.buttonReload.TabIndex = 2;
+            this.buttonReload.Text = "更新";
+            this.buttonReload.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.buttonReload);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelKeyData);
+            this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Opacity = 0.75D;
+            this.Text = "SoundKeyboard 2012";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelKeyData;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonReload;
+    }
+}
+
---
+++ SoundKeyboard2012/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Media;
+using MouseKeyboardActivityMonitor;
+using MouseKeyboardActivityMonitor.WinApi;
+
+namespace SoundKeyboard2012
+{
+    public partial class MainForm : Form
+    {
+        private KeyboardHookListener keyboard_listener;
+
+        public string SoundPackName = string.Empty;
+
+        public string SoundPackPath = string.Empty;
+
+        public string[] SoundPackList = null;
+
+        public Dictionary<string, SoundPlayer> Sounds = new Dictionary<string, SoundPlayer>();
+
+        public void BuildSounds(string name)
+        {
+            SoundPackName = name;
+
+            SoundPackPath = Path.Combine(Application.StartupPath, "Sounds", SoundPackName);
+
+            Sounds = new DirectoryInfo(SoundPackPath)
+                .EnumerateFiles()
+                .Where(_ => _.Extension == ".wav")
+                .Select(_ => new SoundPlayer(_.FullName))
+                .ToDictionary(_ => Path.GetFileNameWithoutExtension(_.SoundLocation));
+        }
+
+        public void BuildSoundsList()
+        {
+            SoundPackList = new DirectoryInfo(Path.Combine(Application.StartupPath, "Sounds"))
+                .GetDirectories()
+                .Select(_ => _.Name)
+                .ToArray();
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            BuildSoundsList();
+            BuildSounds("alpha");
+
+            comboBox1.DataSource = SoundPackList;
+
+            comboBox1.SelectedIndexChanged += (_1, _2) =>
+            {
+                BuildSounds(comboBox1.SelectedItem.ToString());
+            };
+
+            buttonReload.Click += (_1, _2) =>
+            {
+                BuildSoundsList();
+            };
+
+            Load += new EventHandler(MainForm_Load);
+            FormClosed += new FormClosedEventHandler(MainForm_FormClosed);
+        }
+
+        void MainForm_Load(object sender, EventArgs e)
+        {
+            keyboard_listener = new KeyboardHookListener(new GlobalHooker())
+            {
+                Enabled = true,
+            };
+
+            keyboard_listener.KeyDown += (_sender, _e) =>
+            {
+                var key = _e.KeyData.ToString();
+
+                labelKeyData.Text = key;
+
+                if (Sounds.Keys.Contains(key)) Sounds[key].Play();
+            };
+
+            keyboard_listener.KeyUp += (_sender, _e) =>
+            {
+                // labelKeyData.Text = string.Empty;
+            };
+        }
+
+        void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            keyboard_listener.Dispose();
+        }
+    }
+}
